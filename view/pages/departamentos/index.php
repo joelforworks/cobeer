609b9f5a321018cacd018a5c4a373f09e80f4811
joelforworks <joelforworks@gmail.com>
2022-11-23 17:28:38 +0100

    coober

---
+++ view/pages/departamentos/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href="../../layout/layout.css">
+  <title>DEPARTAMENTOS</title>
+</head>
+
+<body>
+  <div class="layout">
+    <header>pàgina departaments
+      <?php include '../../../components/header/component.php' ?>
+    </header>
+
+    <main>
+      <div>
+        <h1>NOM DEL DEPARTAMENT</h1>
+      </div>
+
+      <div>
+        <h5>ARTICLES DESTACATS</h5>
+      </div>
+      <div class="aside">
+        <?php include '../../../components/articulosDestacados/component.php' ?>
+      </div>
+
+      <div class="articulos">
+        <div>
+          <?php include '../../../components/articulo_departamento/component.php' ?>
+        </div>
+
+        <div>
+          <?php include '../../../components/articulo_departamento/component.php' ?>
+        </div>
+      </div>
+    </main>
+
+    <footer>
+      <?php include '../../../components/footer/component.php' ?>
+    </footer>
+  </div>
+</body>
+
+</html>

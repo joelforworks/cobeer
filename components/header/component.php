75db3690564dec307e138eb50253e0f1e4870b68
olgapf02 <olgapf02@gmail.com>
2022-11-23 17:33:04 +0100

    header

--- components/header/component.php
+++ components/header/component.php
@@ -1,7 +1,8 @@
 <style> <?php include __DIR__.'/template.css'; ?> </style>
-
+<header>
 <div class="headerContainer">
     <!-- Componente menuIcon -->
     <img src="cobeer/assets/logo/cobeerLogo.png" height="80" alt="Cobeer Logo"/>
 </div> 
 <div><?php include dirname(__DIR__, 1).'\nav\component.php' ?></div>
+</header>

b721f4ccd103e840de6130142f6a995a7db0ebb0
olgapf02 <olgapf02@gmail.com>
2022-12-01 15:04:07 +0100

    home

--- components/articulosDestacados/component.php
+++ components/articulosDestacados/component.php
@@ -15,12 +15,12 @@
     <div class="flap"></div>
   </div>
 
-  <div class="container">
+  <!-- <div class="container">
     <div class="content">
       <h1>Pratham</h1>
       <?php include dirname(__DIR__, 1).'\resumenArticulo\component.php' ?>  </div>
     <div class="flap"></div>
-  </div>
+  </div> -->
 
   <!-- <div class="container">
     <div class="content">

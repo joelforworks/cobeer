b91555df9a5fe632f593ca5d2f16ca1031ae7f1e
olgapf02 <olgapf02@gmail.com>
2022-12-01 14:43:49 +0100

    paguina de inicio

--- components/articulosDestacados/component.php
+++ components/articulosDestacados/component.php
@@ -2,15 +2,30 @@
     <?php include __DIR__ . '/template.css'; ?>
 </style>
 
-    <div class='slide'>
+    <!-- <div class='slide'>
         <button type="submit">Anterior Artículo</button>
         <button type="submit">Siguiente Artículo</button>
     </div>
-</div>
+</div> -->
+<div>
+  <div class="container">
+    <div class="content">
+      <h1>Pratham</h1>
+      <?php include dirname(__DIR__, 1).'\resumenArticulo\component.php' ?>  </div>
+    <div class="flap"></div>
+  </div>
 
-<div class="container">
-  <div class="content">
-    <h1>Pratham</h1>
-    <?php include dirname(__DIR__, 1).'\resumenArticulo\component.php' ?>  </div>
-  <div class="flap"></div>
-</div>
+  <div class="container">
+    <div class="content">
+      <h1>Pratham</h1>
+      <?php include dirname(__DIR__, 1).'\resumenArticulo\component.php' ?>  </div>
+    <div class="flap"></div>
+  </div>
+
+  <!-- <div class="container">
+    <div class="content">
+      <h1>Pratham</h1>
+      <?php include dirname(__DIR__, 1).'\resumenArticulo\component.php' ?>  </div>
+    <div class="flap"></div>
+  </div>
+</div> -->

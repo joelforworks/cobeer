0b8a5c0d2e7221007268383140f1a12fe99af1d4
olgapf02 <olgapf02@gmail.com>
2022-12-24 12:17:00 +0100

    departamento

--- components/departamento/component.php
+++ components/departamento/component.php
@@ -4,40 +4,40 @@
 </style>
 
 <!-- Este es componente departamento -->
-<div class="container">
-    <div class="tituloDepartemento">
-        <h1>Hola</h1>
-    </div>
-    <div class="articuloDestacados">
+    
         <h1>Articulos destacados</h1>
         <div> <?php include '../../components/articulosDestacados/component.php' ?></div>
 
-
-        <div class="cards">
-                <div class="card">
-                <h1 class="card-title">Articulo-1</h1>
-                <img src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg" alt="">
-                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam quaerat enim ex quisquam maxime, dolor rerum? Voluptatem modi, error, necessitatibus quod temporibus in soluta, beatae eius ratione repudiandae velit
-                    facilis officiis quia unde delectus culpa vero repellat tempora dolor. Quo!</h3>
-        </div>
-<!-- 
-  https://codepen.io/luanmanara/pen/poyLbWx
-  <h1>Card Image with description on :hover</h1>
-<div class="cards">
+        <div class="container">
   <div class="card">
-    <h2 class="card-title">Seal</h2>
-    <img src="https://images.unsplash.com/photo-1591485423007-765bde4139ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt="">
-    <p class="card-desc">Pinnipeds, commonly known as seals,[a] are a widely distributed and diverse clade of carnivorous, fin-footed, semiaquatic marine mammals. They comprise the extant families Odobenidae (whose only living member is the walrus), Otariidae (the eared seals: sea lions and fur seals), and Phocidae (the earless seals, or true seals).</p>
-  </div>
-</div> --> 
+        <div class="card-header">
+        <img src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg" alt="rover" />
         </div>
+        <div class="card-body">
+        <span class="tag tag-teal">cerveza</span>
+        <h4>
+            Como se hace la cerveza?
+        </h4>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam quaerat enim ex quisquam maxime, dolor rerum? Voluptatem modi, error, necessitatibus quod temporibus in soluta, beatae eius ratione repudiandae velit
+            facilis officiis quia unde delectus culpa vero repellat tempora dolor. Quo!
+        </p>
+        </div>
+      </div>
     </div>
-    <!-- <div class="articulos">
-        <div class="articuloss">
-            <img class="imgArticulo" src="" alt="">
-            <h1 class="tituloArticulo"></h1>
-            <p class="detailesArticulos"></p>
-        </div>
-    </div> -->
-</div>
+  </div>
+
+        <!-- <div class="cards">
+            <div class="card">
+            <h1 class="card-title">Articulo-3</h1>
+            <img src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg" alt="">
+            <p class="card-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam quaerat enim ex quisquam maxime, dolor rerum? Voluptatem modi, error, necessitatibus quod temporibus in soluta, beatae eius ratione repudiandae velit
+            facilis officiis quia unde delectus culpa vero repellat tempora dolor. Quo!</p>
+        </div> -->
+
+    </div>
+
+
+
+
 

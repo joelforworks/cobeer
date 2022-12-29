f035b907c85f03d6f59d6a8765f467d46f3662a8
olgapf02 <olgapf02@gmail.com>
2022-12-29 21:09:04 +0100

    articulos en el home

--- components/articulosDestacados/component.php
+++ components/articulosDestacados/component.php
@@ -72,7 +72,126 @@
         <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
       </div>
     </a>
-  </li>    
+  </li>  
+
+  <li>
+    <a href="" class="card">
+      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+      <div class="card__overlay">
+        <div class="card__header">
+          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
+          <div class="card__header-text">
+            <h3 class="card__title">Articulo-5</h3>
+          </div>          
+        </div>
+        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
+      </div>
+    </a>
+  </li>   
+  
+  <li>
+    <a href="" class="card">
+      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+      <div class="card__overlay">
+        <div class="card__header">
+          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
+          <div class="card__header-text">
+            <h3 class="card__title">Articulo-6</h3>
+          </div>          
+        </div>
+        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
+      </div>
+    </a>
+  </li> 
+
+  <li>
+    <a href="" class="card">
+      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+      <div class="card__overlay">
+        <div class="card__header">
+          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
+          <div class="card__header-text">
+            <h3 class="card__title">Articulo-7</h3>
+          </div>          
+        </div>
+        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
+      </div>
+    </a>
+  </li> 
+
+  <li>
+    <a href="" class="card">
+      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+      <div class="card__overlay">
+        <div class="card__header">
+          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
+          <div class="card__header-text">
+            <h3 class="card__title">Articulo-8</h3>
+          </div>          
+        </div>
+        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
+      </div>
+    </a>
+  </li> 
+  <li>
+    <a href="" class="card">
+      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+      <div class="card__overlay">
+        <div class="card__header">
+          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+          <div class="card__header-text">
+            <h3 class="card__title">Articulo-1</h3>            
+          </div>
+        </div>
+        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
+      </div>
+    </a>      
+  </li>
+
+  <li>
+    <a href="" class="card">
+      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+      <div class="card__overlay">        
+        <div class="card__header">
+          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
+          <div class="card__header-text">
+            <h3 class="card__title">Articulo-2</h3>
+          </div>
+        </div>
+        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
+      </div>
+    </a>
+  </li>
+
+  <li>
+    <a href="" class="card">
+      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+      <div class="card__overlay">
+        <div class="card__header">
+          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+          <div class="card__header-text">
+            <h3 class="card__title">Articulo-3</h3>         
+          </div>
+        </div>
+        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
+      </div>
+    </a>
+  </li>
+
+  <li>
+    <a href="" class="card">
+      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+      <div class="card__overlay">
+        <div class="card__header">
+          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
+          <div class="card__header-text">
+            <h3 class="card__title">Articulo-4</h3>
+          </div>          
+        </div>
+        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
+      </div>
+    </a>
+  </li>  
 </ul>
 
 <!-- <div>
--- components/resumenArticulo/component.php
+++ components/resumenArticulo/component.php
@@ -3,18 +3,18 @@
 </style>
 <div class="RMarticleContariner">
 	<div class="RMtittle">
-		<h1>Hola</h1>
 	</div>
 	<div class="RMarticleDestacados">
 	<div class="RMparagraph">
+		<h1>Lorem</h1>
 		<p>
 			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
 			standard dummy text ever since 
 			the 1500s.
 		</p>
 	</div>
-	<div class="RMimagen">
+	<!-- <div class="RMimagen">
 		<img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" width="50" alt="">
-	</div>
+	</div> -->
 	</div>
 </div>

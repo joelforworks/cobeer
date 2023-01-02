5653095a5e0e39b01b4490cc046ab52614977f19
olgapf02 <olgapf02@gmail.com>
2023-01-02 09:32:50 +0100

    departamento y articulos

--- components/departamento/component.php
+++ components/departamento/component.php
@@ -4,38 +4,12 @@
 </style>
 
 <!-- Este es componente departamento -->
-    
-        <h1>Articulos destacados</h1>
+
+<body>  
         <div> <?php include '../../components/articulosDestacados/component.php' ?></div>
+</body>
 
-        <div class="container">
-  <div class="card">
-        <div class="card-header">
-        <img src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg" alt="rover" />
-        </div>
-        <div class="card-body">
-        <span class="tag tag-teal">cerveza</span>
-        <h4>
-            Como se hace la cerveza?
-        </h4>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam quaerat enim ex quisquam maxime, dolor rerum? Voluptatem modi, error, necessitatibus quod temporibus in soluta, beatae eius ratione repudiandae velit
-            facilis officiis quia unde delectus culpa vero repellat tempora dolor. Quo!
-        </p>
-        </div>
-      </div>
-    </div>
-  </div>
 
-        <!-- <div class="cards">
-            <div class="card">
-            <h1 class="card-title">Articulo-3</h1>
-            <img src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg" alt="">
-            <p class="card-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam quaerat enim ex quisquam maxime, dolor rerum? Voluptatem modi, error, necessitatibus quod temporibus in soluta, beatae eius ratione repudiandae velit
-            facilis officiis quia unde delectus culpa vero repellat tempora dolor. Quo!</p>
-        </div> -->
-
-    </div>
 
 
 

9de0b7fcf9816dc31c8d59d715162781a7949210
joelforworks <joelforworks@gmail.com>
2023-01-04 16:57:55 +0100

    mapa

--- components/mapa/component.php
+++ components/mapa/component.php
@@ -1,3 +1,15 @@
+<style>
+	<?php include 'template.css'?>
+</style>
 <div class="mapa">
-	
+	<img src="../../../assets/imagenes/mapa-1.png" alt="" width="674" height="490" usemap="#image-map">
+	<div class="trigo">
+		trigo
+	</div>
+	<map name="image-map">
+    <area target="" alt="" title=""  coords="0,125,88,0" shape="rect">
+	</map>
+	<script>
+		<?php include 'script.js'?>
+	</script>
 </div>

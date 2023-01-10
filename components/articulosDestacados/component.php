11abfeed996a89326b412de7fbcd3b5ed9aedc5b
joelforworks <101220133+joelforworks@users.noreply.github.com>
2023-01-10 18:33:09 +0100

    articulos destacados componente

--- components/articulosDestacados/component.php
+++ components/articulosDestacados/component.php
@@ -1,192 +1,186 @@
 <style>
     <?php include __DIR__ . '/template.css'; ?>
 </style>
-<header>
-  <?php include dirname(__DIR__, 1).'/header/component.php' ?>
-</header>
-<body>
-<ul class="cards">
-  <li>
-    <a href="" class="card">
-      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <div class="card__header-text">
-            <h3 class="card__title">Articulo-1</h3>            
+<div>
+  <ul class="cards">
+    <li>
+      <a href="" class="card">
+        <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+            <div class="card__header-text">
+              <h3 class="card__title">Articulo-1</h3>            
+            </div>
           </div>
+          <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
         </div>
-        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
-      </div>
-    </a>      
-  </li>
+      </a>      
+    </li>
 
-  <li>
-    <a href="" class="card">
-      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
-      <div class="card__overlay">        
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
-          <div class="card__header-text">
-            <h3 class="card__title">Articulo-2</h3>
+    <li>
+      <a href="" class="card">
+        <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+        <div class="card__overlay">        
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
+            <div class="card__header-text">
+              <h3 class="card__title">Articulo-2</h3>
+            </div>
           </div>
+          <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
         </div>
-        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
-      </div>
-    </a>
-  </li>
+      </a>
+    </li>
 
-  <li>
-    <a href="" class="card">
-      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <div class="card__header-text">
-            <h3 class="card__title">Articulo-3</h3>         
+    <li>
+      <a href="" class="card">
+        <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+            <div class="card__header-text">
+              <h3 class="card__title">Articulo-3</h3>         
+            </div>
           </div>
+          <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
         </div>
-        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
-      </div>
-    </a>
-  </li>
+      </a>
+    </li>
 
-  <li>
-    <a href="" class="card">
-      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
-          <div class="card__header-text">
-            <h3 class="card__title">Articulo-4</h3>
-          </div>          
-        </div>
-        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
-      </div>
-    </a>
-  </li>  
-
-  <li>
-    <a href="" class="card">
-      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
-          <div class="card__header-text">
-            <h3 class="card__title">Articulo-5</h3>
-          </div>          
-        </div>
-        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
-      </div>
-    </a>
-  </li>   
-  
-  <li>
-    <a href="" class="card">
-      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
-          <div class="card__header-text">
-            <h3 class="card__title">Articulo-6</h3>
-          </div>          
-        </div>
-        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
-      </div>
-    </a>
-  </li> 
-
-  <li>
-    <a href="" class="card">
-      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
-          <div class="card__header-text">
-            <h3 class="card__title">Articulo-7</h3>
-          </div>          
-        </div>
-        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
-      </div>
-    </a>
-  </li> 
-
-  <li>
-    <a href="" class="card">
-      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
-          <div class="card__header-text">
-            <h3 class="card__title">Articulo-8</h3>
-          </div>          
-        </div>
-        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
-      </div>
-    </a>
-  </li> 
-  <li>
-    <a href="" class="card">
-      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <div class="card__header-text">
-            <h3 class="card__title">Articulo-1</h3>            
+    <li>
+      <a href="" class="card">
+        <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
+            <div class="card__header-text">
+              <h3 class="card__title">Articulo-4</h3>
+            </div>          
           </div>
+          <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
         </div>
-        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
-      </div>
-    </a>      
-  </li>
+      </a>
+    </li>  
 
-  <li>
-    <a href="" class="card">
-      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
-      <div class="card__overlay">        
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
-          <div class="card__header-text">
-            <h3 class="card__title">Articulo-2</h3>
+    <li>
+      <a href="" class="card">
+        <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
+            <div class="card__header-text">
+              <h3 class="card__title">Articulo-5</h3>
+            </div>          
           </div>
+          <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
         </div>
-        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
-      </div>
-    </a>
-  </li>
-
-  <li>
-    <a href="" class="card">
-      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <div class="card__header-text">
-            <h3 class="card__title">Articulo-3</h3>         
+      </a>
+    </li>   
+    
+    <li>
+      <a href="" class="card">
+        <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
+            <div class="card__header-text">
+              <h3 class="card__title">Articulo-6</h3>
+            </div>          
           </div>
+          <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
         </div>
-        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
-      </div>
-    </a>
-  </li>
+      </a>
+    </li> 
 
-  <li>
-    <a href="" class="card">
-      <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">
-          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
-          <div class="card__header-text">
-            <h3 class="card__title">Articulo-4</h3>
-          </div>          
+    <li>
+      <a href="" class="card">
+        <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
+            <div class="card__header-text">
+              <h3 class="card__title">Articulo-7</h3>
+            </div>          
+          </div>
+          <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
         </div>
-        <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
-      </div>
-    </a>
-  </li>  
-</ul>
-</body>
-<footer>
-  <?php include dirname(__DIR__, 1).'/footer/component.php' ?>
-</footer>
+      </a>
+    </li> 
+
+    <li>
+      <a href="" class="card">
+        <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
+            <div class="card__header-text">
+              <h3 class="card__title">Articulo-8</h3>
+            </div>          
+          </div>
+          <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
+        </div>
+      </a>
+    </li> 
+    <li>
+      <a href="" class="card">
+        <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+            <div class="card__header-text">
+              <h3 class="card__title">Articulo-1</h3>            
+            </div>
+          </div>
+          <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
+        </div>
+      </a>      
+    </li>
+
+    <li>
+      <a href="" class="card">
+        <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+        <div class="card__overlay">        
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
+            <div class="card__header-text">
+              <h3 class="card__title">Articulo-2</h3>
+            </div>
+          </div>
+          <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
+        </div>
+      </a>
+    </li>
+
+    <li>
+      <a href="" class="card">
+        <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+            <div class="card__header-text">
+              <h3 class="card__title">Articulo-3</h3>         
+            </div>
+          </div>
+          <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
+        </div>
+      </a>
+    </li>
+
+    <li>
+      <a href="" class="card">
+        <img src="https://summitsoft.com/wp-content/uploads/2014/07/Blue-moon-beer-logo.jpg" class="card__image" alt="" />
+        <div class="card__overlay">
+          <div class="card__header">
+            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
+            <div class="card__header-text">
+              <h3 class="card__title">Articulo-4</h3>
+            </div>          
+          </div>
+          <p class="card__description"><?php include dirname(__DIR__, 1).'/resumenArticulo/component.php' ?></p>
+        </div>
+      </a>
+    </li>  
+  </ul>
+</div>
 

a023c11c561e9ff288fdce968f262093e8f25025
olgapf02 <olgapf02@gmail.com>
2023-01-10 17:59:31 +0100

    departamentos

--- view/pages/departamentos/index.php
+++ view/pages/departamentos/index.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+<!-- <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="styles.css">
   <link rel="stylesheet" href="../../layout/layout.css">
   <title>DEPARTAMENTOS</title>
-</head>
+</head> -->
 
 <body>
   <div class="layout">
 
-    <header>pàgina departaments
+    <header>
     <div><?php include '../../../components/header/component.php'?></div>
     </header>
 
